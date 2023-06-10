ac76995e780b269ff5767bb7b4173e788a53383a
yeop12 <duq47@naver.com>
2023-06-10 16:53:48 +0900

    Add auto disposer in pool object

--- Assets/Plugins/UniEx/AssetPool/PoolObject.cs
+++ Assets/Plugins/UniEx/AssetPool/PoolObject.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace UniEx
 {
 	public class PoolObject : MonoBehaviour
 	{
+		private readonly List<IDisposable> _disposers = new();
 		public Transform CachedTransform { get; private set; }
 
 		public event Action<PoolObject> OnReturn;
@@ -19,10 +21,17 @@ namespace UniEx
 			gameObject.SetActive(true);
 		}
 
+		protected void AddDisposer(IDisposable disposer)
+		{
+			_disposers.Add(disposer);
+		}
+
 		public virtual void Return()
 		{
 			gameObject.SetActive(false);
 			OnReturn?.Invoke(this);
+			_disposers.ForEach(x => x.Dispose());
+			_disposers.Clear();
 		}
 	}
 }
