7596f4cf2e312909ffb87457d5636f1b06a1b88b
yeop12 <duq47@naver.com>
2024-06-12 07:23:27 +0900

    Add dropdown binder to withoutnotify parameter

--- Assets/Plugins/UniEx/UI/Binder/DropdownBinder.cs
+++ Assets/Plugins/UniEx/UI/Binder/DropdownBinder.cs
@@ -8,12 +8,14 @@ namespace UniEx.UI
 	public class DropdownBinder : SelectableBinder<TMP_Dropdown>
 	{
 		[SerializeField] private string _valueParameterName;
+		[SerializeField] private string _valueWithoutNotifyParameterName;
 		[SerializeField] private string _optionsParameterName;
 
 		protected override void Awake()
 		{
 			base.Awake();
 			AddParameter<int>(_valueParameterName, x => UIComponent.value = x);
+			AddParameter<int>(_valueWithoutNotifyParameterName, x => UIComponent.SetValueWithoutNotify(x));
 			AddParameter<List<TMP_Dropdown.OptionData>>(_optionsParameterName, x => UIComponent.options = x);
 		}
 	}
