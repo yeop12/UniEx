ddd8c80ca74365256e86b0f667522698ac98d824
yeop12 <duq47@naver.com>
2025-04-26 13:39:47 +0900

    Rename OnGetFunction to OnGetImpl and change Model to public property

--- Assets/Plugins/UniEx/AssetPool/PoolObjectTemplate.cs
+++ Assets/Plugins/UniEx/AssetPool/PoolObjectTemplate.cs
@@ -2,16 +2,16 @@ namespace UniEx
 {
 	public class PoolObjectTemplate<T> : PoolObject
 	{
-		protected T Model { get; private set; }
+		public T Model { get; private set; }
 
 		protected internal sealed override void OnGet(object modelObject)
 		{
 			base.OnGet(modelObject);
 			Model = (T)modelObject;
-			OnGet(Model);
+			OnGetImpl(Model);
 		}
 
-		protected virtual void OnGet(T model)
+		protected virtual void OnGetImpl(T model)
 		{
 
 		}
